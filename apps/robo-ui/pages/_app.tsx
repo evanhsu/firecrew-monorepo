499c5e6e95dbf24acf4226d48bbaf08018678251
Evan Hsu <evanhsu@gmail.com>
2022-10-08 08:34:56 -0600

    Add useDeviceTouchscreenDetector hook to robo-ui

--- apps/robo-ui/pages/_app.tsx
+++ apps/robo-ui/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to robo-ui!</title>
+        <title>RoBo</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
