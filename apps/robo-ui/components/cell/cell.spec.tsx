9f831251ef8e9f3e6204c7fae2719fd7ddfdef51
Evan Hsu <evanhsu@gmail.com>
2022-10-08 08:52:46 -0600

    Stubbed out Cell component

---
+++ apps/robo-ui/components/cell/cell.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import Cell from './cell';
+
+describe('Cell', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Cell />);
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ apps/robo-ui/components/cell/cell.tsx
@@ -0,0 +1,58 @@
+import styled from '@emotion/styled';
+
+/* eslint-disable-next-line */
+export interface CellProps {}
+
+const StyledCell = styled.div`
+  color: pink;
+`;
+
+export function Cell(props: CellProps) {
+  // const [moveTile] = useMutation(MOVE_TILE);
+  // const [sendBoardState] = useMutation(SET_BOARD_STATE)
+  // const [, drop] = useDrop({
+  //     accept: 'TILE',
+  //     hover(item: DragItem, monitor: DropTargetMonitor) {
+  //         const startPosition = item.position;
+  //         const hoverPosition = {
+  //             row,
+  //             column,
+  //         };
+
+  //         // Don't replace items with themselves
+  //         if (coordinatesAreEqual(startPosition, hoverPosition)) {
+  //             return
+  //         }
+
+  //         // Time to actually perform the action
+  //         console.log(`${JSON.stringify(startPosition)}, ${JSON.stringify(hoverPosition)}`);
+  //         boardState = moveTile({ variables: {startPosition, hoverPosition} });
+
+  //         item.position = hoverPosition;
+  //     },
+  //     drop(item: DragItem, monitor: DropTargetMonitor) {
+  //         // Send the entire board state to the API here: "mutation setBoardState"
+  //         console.log('DROPPED');
+  //         boardState.then((board: any) => {
+  //             const trimmedRows = board.data?.moveTile?.state?.rows.map((row: any) => {
+  //                 let trimmedRow = {...row}; // Clone this object
+  //                 delete trimmedRow.__typename;
+  //                 delete trimmedRow.person.__typename;
+  //                 return trimmedRow;
+  //             });
+
+  //             // const setBoardStateResponse = sendBoardState({ variables: { boardState: trimmedRows }})
+  //             // setBoardStateResponse.then((data) => console.log(data));
+  //         });
+  //     },
+  // });
+
+  return (
+    <StyledCell>This is a Cell</StyledCell>
+    // <div ref={drop} style={styles}>
+    //     {tile ? (<Tile primaryText={tile.name} secondaryText={String(tile.id)} id={tile.id} position={{ row, column }} />) : ''}
+    // </div>
+  );
+}
+
+export default Cell;
