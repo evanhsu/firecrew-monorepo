8779c072e2edda785fa440c6bed65919eab19112
Evan Hsu <evanhsu@gmail.com>
2022-10-08 10:06:56 -0600

    Stubbed out Row component in robo-ui

---
+++ apps/robo-ui/components/row/row.tsx
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+import Cell from '../cell/cell';
+
+export interface RowProps {
+  person: {
+    id: number;
+    name: string;
+  };
+  activeColumn: number;
+  index: number;
+}
+
+const StyledRow = styled.div`
+    display: 'flex',
+    flexDirection: 'row',
+    height: '75px',
+`;
+
+export function Row(props: RowProps) {
+  const { person, activeColumn, index } = props;
+  const cells = [0, 1, 2].map((_, i) => {
+    return (
+      <Cell
+        tile={i === activeColumn ? person : undefined}
+        row={index}
+        column={i}
+        key={`${person.id}${i}`}
+      />
+    );
+  });
+
+  return <StyledRow>{cells}</StyledRow>;
+}
+
+export default Row;
