832f7b67e525a29ec9738b6ad99621b4efdcf4e6
Evan Hsu <evanhsu@gmail.com>
2022-10-21 09:04:56 -0600

    Created typography theme

---
+++ apps/robo-ui/theme/typography.ts
@@ -0,0 +1,15 @@
+import { TypographyOptions } from '@mui/material/styles/createTypography';
+/**
+ * Any fonts that are referenced here should be imported at the top of `/pages/_app.tsx`
+ */
+export const typography: TypographyOptions = {
+    h3: {
+        fontSize: 24,
+    },
+    subtitle1: {
+        fontSize: 16,
+        fontFamily: 'Roboto',
+    },
+};
+
+export default typography;
